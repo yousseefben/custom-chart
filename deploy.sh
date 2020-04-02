aa3e1ca580ad8ed91b4b9b9820033e2035a97c3e
Youssef Bentaleb <ybentaleb@octo.com>
2020-04-02 18:15:07 +0100

    some update

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ git commit -m "some update"
 git push
 git show gh-pages:index.yaml > index.yaml  
 helm package .
+helm repo index . --url https://yousseefben.github.io/custom-chart/ 
 git checkout gh-pages -f
 git pull
-helm repo index . --url https://yousseefben.github.io/custom-chart/ 
 git add index.yaml
 git commit -m "some update"
 git push 
